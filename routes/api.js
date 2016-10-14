9668c8cdd7c787e7cce9d6fc89746bf6fb348bd3
Navjot Singh <weavebytes@gmail.com>
2016-10-14 15:09:30 +0530

    Login API

--- routes/api.js
+++ routes/api.js
@@ -1,14 +1,26 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
+/* api end points reference page. */
 router.get('/', function(req, res, next) {
   res.render('api_demo', { title: 'API Endpoints Are' });
 });
 
-/* GET home page. */
+/* api demo page */
 router.get('/demo', function(req, res, next) {
   res.render('api_demo', { title: 'API Demo' });
+});
+
+/* api login */
+router.post('/login', function(req, res, next) {
+    username = req.body.Username;
+    password = req.body.Password;
+
+    console.log('username:' +  username);
+    console.log('password:' +  password);
+
+    //res.render('api_demo', { title: 'API Demo' });
+    res.send('Loggin...')
 });
 
 module.exports = router;
